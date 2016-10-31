093ef70ba6d3f5d14734a3e65a5fca0139d7c9db
内藤 謙一 <fine14r3@gmail.com>
2016-10-31 14:46:46 +0900

    First Commit

---
+++ sample001/src/js/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    )
+  }
+}
+
+
+
+module.exports = {
+  run: () => {
+    ReactDOM.render(
+      <Hello />,
+      document.getElementById('hello')
+    )
+  }
+}
+
+
